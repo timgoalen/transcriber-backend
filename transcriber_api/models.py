88e9d1f004cc937ec584be2f1468498c0d18589f
Tim Goalen <timgoalen@hotmail.com>
2023-12-12 15:26:39 +0000

    Add note model

--- transcriber_api/models.py
+++ transcriber_api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Note(models.Model):
+    note = models.TextField()
+    timestamp = models.IntegerField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now = True, blank = True)
+    # user = models.ForeignKey(User, on_delete = models.CASCADE, blank = True, null = True)
+
+    # class Meta:
+    #     ordering = ["-created_on"]
+    
+    def __str__(self):
+        return self.note
