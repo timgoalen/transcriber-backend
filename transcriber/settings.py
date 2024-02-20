edd42b555d4112435a5bef69c37a165d731a198f
Tim Goalen <timgoalen@hotmail.com>
2024-02-20 14:14:31 +0000

    chore: set debug to false

--- transcriber/settings.py
+++ transcriber/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*", "https://3000-timgoalen-transcriberfr-h1tyvl9vsqe.ws-eu108.gitpod.io/",
 "https://transcriber-frontend.vercel.app/"]
